ad46213b2dc0e5e020e8d3aa27ef0b80ca0ffa3a
Ricardo Fabio Caterino Polini <ricardocaterino@gmail.com>
2022-12-02 09:44:58 +0100

    Comunicación full duplex establecida

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Servidor de Express
-const app = require("express")();
+const express = require("express");
+const { addAbortSignal } = require("stream");
+const app = express();
 
 //Servidor de Socket
 const server = require("http").createServer(app);
@@ -7,12 +9,14 @@ const server = require("http").createServer(app);
 //Configuración del Socket Server
 const io = require("socket.io")(server);
 
+//Desplegar el directorio público
+app.use( express.static(__dirname + "/public") );
 
 io.on("connection", () => {
   /* … */
+  console.log("cliente conectado");
 });
 
-
 server.listen(8080, () => {
-    console.log('Server corriendo en puerto :8080');
+  console.log("Server corriendo en puerto :8080");
 });
