c273cc0f879aa9880a307cea208610314bb40dda
Ricardo Fabio Caterino Polini <ricardocaterino@gmail.com>
2022-12-12 09:58:01 +0100

    Encuchar y emitir eventos en el cliente y servidor

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 //Desplegar el directorio público
-app.use( express.static(__dirname + "/public") );
+app.use(express.static(__dirname + "/public"));
 
-io.on("connection", () => {
+//A continuación, con el método on, el servidor escucha lo que recibe del front-end
+io.on("connection", (socket) => {
   /* … */
-  console.log("cliente conectado");
+  console.log(socket.id);
+
+  //A continuación, con el métido emit, emitimos información desde el servidor hacia el front-end
+  //"mensaje-bienvenida" es el nombre del evento que queremos emitir.
+  //el siguiente parametro es un objeto, lo hacemos con este ejemplo porque por lo general siempre queremos emitir desde el servidor mucha información
+  socket.emit("mensaje-bienvenida", {
+    msg: "Bienvenido al server",
+    fecha: new Date(),
+  });
+
+  //escuchamos evento en el servidor
+  socket.on("mensaje-cliente", (data) => {
+    console.log(data);
+  });
 });
 
 server.listen(8080, () => {
